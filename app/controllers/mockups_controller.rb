8c3f683c015f7579032f55c1656c7a19fbea89cf
Awin Abi <awin@awinawin.awin>
2020-04-01 20:00:50 +0530

    Add controller to serve mockups of views

---
+++ app/controllers/mockups_controller.rb
@@ -0,0 +1,13 @@
+class MockupsController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def category
+  end
+
+  def cart
+  end
+end
---
+++ spec/requests/mockups_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Mockups", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/mockups/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /search" do
+    it "returns http success" do
+      get "/mockups/search"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /category" do
+    it "returns http success" do
+      get "/mockups/category"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /cart" do
+    it "returns http success" do
+      get "/mockups/cart"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
