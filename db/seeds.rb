966c1fae4b4db5c1aec3504582abe1963a280791
Awin Abi <awin@awinawin.awin>
2020-04-07 18:45:23 +0530

    Add seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'Admin User', email: 'admin@example.com', password: 'mangointernet', admin: true)
+User.create!(name: 'John Doe', email: 'user@example.com', password: 'mangointernet')
+User.create!(name: 'Jane Roe', email: 'jane@example.com', password: 'mangointernet')
