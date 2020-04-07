589fb2a0b692029b08807675c653c5361f699a1e
Joker <yadav666.yg@gmail.com>
2020-04-07 18:14:36 +0530

    admin dashboard access -admins only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,8 @@
 
 # Admin AdminController class
 class AdminController < ApplicationController
+  def authenticate_admin
+    redirect_to new_user_session_path unless current_user.admin?
+  end
+  helper_method :authenticate_admin
 end
