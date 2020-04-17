9dc42ae3b71ccab2ff9af9e3c0efda9b8dea09f0
Joker <yadav666.yg@gmail.com>
2020-04-17 17:26:03 +0530

    changes in shipping methods

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class ShippingMethod < ApplicationRecord
-  validates :name, presence: true
-  validates :code, presence: true
-  validates :value_type, presence: true
-  validates :value, presence: true
+  validates :name, :code, :value, :value_type, presence: true
 end
--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -3,27 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe ShippingMethod, type: :model do
-  before :each do
-    @shipping_method = create(:shipping_method)
-  end
+  subject { create(:shipping_method) }
 
   it 'should have a name' do
-    @shipping_method.name = nil
-    expect(@shipping_method).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
   it 'should have a code' do
-    @shipping_method.code = nil
-    expect(@shipping_method).to_not be_valid
+    subject.code = nil
+    expect(subject).to_not be_valid
   end
 
   it 'should have a value_type' do
-    @shipping_method.value_type = nil
-    expect(@shipping_method).to_not be_valid
+    subject.value_type = nil
+    expect(subject).to_not be_valid
   end
 
   it 'should have a value' do
-    @shipping_method.value = nil
-    expect(@shipping_method).to_not be_valid
+    subject.value = nil
+    expect(subject).to_not be_valid
   end
 end
