174c77e4323afb50f2a19e32869cf8896449b6e3
Awin Abi <awin@awinawin.awin>
2020-04-17 09:50:24 +0530

    Update database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_04_16_061026) do
 
   # These are extensions that must be enabled in order to support this database
-  # pgsql
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
