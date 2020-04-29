006a7949063f48d8422e13168510e928f8e2a80c
Joker <yadav666.yg@gmail.com>
2020-04-29 19:38:41 +0530

    otp_valid_till

--- app/models/otp.rb
+++ app/models/otp.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
 
 class Otp < ApplicationRecord
-  scope :valid, -> { where('valid_till > ?', Time.zone.now) }
+  scope :expired, -> { where('valid_till < ?', Time.zone.now) }
   belongs_to :user
   validates :code, presence: true
-  before_save :valid_untill
 
   def self.generate_otp
     '%06d' % rand(1_000_000)
   end
 
-  def valid_untill
-    self.valid_till = 1.hour.from_now
-  end
-
-  def self.expired_otps
-    where('valid_till <= ?', Time.zone.now)
+  def expired?
+    valid_till < Time.zone.now
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,16 +24,16 @@ class User < ApplicationRecord
 
   def generate_otp_and_notify
     otp = Otp.generate_otp
-    otps.create!(code: otp)
+    otps.create!(code: otp, valid_till: 1.hour.from_now)
     UserMailer.with(user: self, otp: otp).login_otp.deliver_now
   end
 
   def verify_otp_and_save(otp)
-    return unless otps.valid.find_by(code: otp)
+    otp = otps.find_by(code: otp)
+    return if otp.nil? || otp.expired?
 
-    user_otp = otps.valid.find_by(code: otp)
-    user_otp.verified = true
-    user_otp.save
+    otp.verified = true
+    otp.save
   end
 
   private
