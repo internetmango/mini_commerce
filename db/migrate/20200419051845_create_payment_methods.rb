8008c24eab4f4d9cf7b64f46975cc9c4aef45f7b
Awin Abi <awin@awinawin.awin>
2020-05-07 01:16:29 +0530

    Fix migration for payment_methods/code

--- db/migrate/20200419051845_create_payment_methods.rb
+++ db/migrate/20200419051845_create_payment_methods.rb
@@ -4,7 +4,7 @@ class CreatePaymentMethods < ActiveRecord::Migration[6.0]
       t.string :name, null:false
       t.text :description
       t.string :provider, null:false
-      t.code :string
+      t.string :code
       t.boolean :active, null:false, default: false
 
       t.timestamps
