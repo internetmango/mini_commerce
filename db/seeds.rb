0026e4b692ea973ab910f4d8270e775232635d76
Awin Abi <awin@awinawin.awin>
2020-05-07 13:34:24 +0530

    Light ui theme, some fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 User.create!(name: 'Admin User', email: 'admin@example.com', password: 'mangointernet', admin: true)
 User.create!(name: 'John Doe', email: 'user@example.com', password: 'mangointernet')
 User.create!(name: 'Jane Roe', email: 'jane@example.com', password: 'mangointernet')
+
 category = Category.create!(name: 'Phone')
 products = Product.create!(
   [
