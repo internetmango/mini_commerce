c12280207dd3907bd4099ddc7a881dd136cc83dd
Awin Abi <awin@awinawin.awin>
2020-05-17 18:26:34 +0530

    Storefront theme for Mini-Shop

---
+++ app/controllers/storefront/orders_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Storefront
+  class OrdersController < StorefrontController
+    def cart; end
+
+    def confirm; end
+  end
+end
---
+++ app/controllers/storefront/products_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Storefront
+  class ProductsController < StorefrontController
+    def index; end
+
+    def show; end
+  end
+end
